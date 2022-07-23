c3068550a9041a0b966fc67c3325e12011b7484a
Samantha Summerhays <samantha.summerhays@gmail.com>
2022-07-23 12:52:38 -0600

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function() {
-
+repeatString('hey', 3)
+  let string = (repeatString);
+  (i = 3; i < string.length; i++)
+  return string = function(i);
+  
 };
 
 // Do not edit below this line
